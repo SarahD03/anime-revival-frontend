3ff6051abb41d6d659255f07d197a6be11f843c7
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-12-10 13:30:24 -0500

    app functions added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
 import './App.css'
+import { CheckSession } from './services/Auth'
+import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
       <h1>Test</h1>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </main>
     </div>
   )
 }
