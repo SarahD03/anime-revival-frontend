f6ec07a70c96da278b7409009a3ebcce3b1d8ad0
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-12-11 13:23:15 -0500

    feed functioning

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,7 +1,30 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { BASE_URL } from '../services/api'
+
 const Feed = ({ user, authenticated }) => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const handlePosts = async () => {
+      const feed = await axios.get(`${BASE_URL}/posts/all`)
+      setPosts(feed.data)
+      console.log(feed.data)
+    }
+    handlePosts()
+  }, [])
+
   return user && authenticated ? (
     <div>
       <h3>Welcome to your feed!</h3>
+      <div>
+        {posts.map((item) => (
+          <div className="post-card">
+            <img src={item.image} />
+            <h3>{item.description}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   ) : (
     <div>
