461a80c02e758cdf2879144fa701b5f5243d3830
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-12-12 13:32:45 -0500

    added css to view post

--- client/src/components/ViewPost.jsx
+++ client/src/components/ViewPost.jsx
@@ -1,9 +1,36 @@
-const ViewPost = () => {
+import { useState, useEffect } from "react"
+import Client from '../services/api'
+import { useParams } from "react-router-dom"
 
 
+const ViewPost = (props) => {
+
+
+let { id } = useParams()
+
+// const [comments, setComments] = useState(props)
+
+// useEffect(() => {
+//     const getPosts = async () => {
+//       let commentSec = await Client.get(`/posts/${id}`)
+//       setComments(commentSec.data.comments)
+//       console.log('comment data:', commentSec.data.comments)
+//     }
+//     getPosts()
+//   }, [])
+
+const comments = props.post.comments.map((comment) => comment.content)
 
     return(
-        <div></div>
+        <div className="view-post" >
+        <h3>
+        Post Details
+        </h3>
+            <h2>{props.post.description}</h2>
+            <img src={props.post.image} style={{ width: '20em', border: '2px solid grey' }}/>
+            <h4>Comments:</h4>
+            <h3>- {comments}</h3>
+        </div>
     )
 }
 
