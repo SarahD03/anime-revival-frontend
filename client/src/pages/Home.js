06cecd77f04291dec0188ffeb4404ba4f0d51d63
Sarah Duperoux <Duperouxsarah@gmail.com>
2022-12-14 15:12:45 -0500

    deploying

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,6 +20,7 @@ const Home = () => {
               borderRadius: '15px',
               boxShadow: 'black 3px 6px 9px'
             }}
+            alt="anime cover"
             src="https://www.boredpanda.com/blog/wp-content/uploads/2021/11/best-anime-series-28-61a6373893e83__700.jpg"
           />
         </div>
@@ -34,6 +35,7 @@ const Home = () => {
               borderRadius: '15px',
               boxShadow: 'black 3px 6px 9px'
             }}
+            alt="anime cover"
             src="https://qph.cf2.quoracdn.net/main-qimg-dcd985f5a5a3688236dea1c744bf18b9.webp"
           />
         </div>
@@ -62,6 +64,7 @@ const Home = () => {
               borderRadius: '15px',
               boxShadow: 'black 3px 6px 9px'
             }}
+            alt="anime cover"
             src="https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781421515397/nana-vol-8-9781421515397_hr.jpg"
           />
         </div>
@@ -76,6 +79,7 @@ const Home = () => {
               borderRadius: '15px',
               boxShadow: 'black 3px 6px 9px'
             }}
+            alt="anime cover"
             src="https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/9f93bb58e47e5ca59d0980de6351eac9.jpeg"
           />
         </div>
